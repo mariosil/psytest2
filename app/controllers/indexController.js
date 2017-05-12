4e079bb38d387d076dfe768beab50285c10e9bc1
Vexed-M <silveiramariolab@gmail.com>
2017-05-12 13:44:46 -0500

    md-card in home view was replaced by a md-content. The Settings  and List FAB Toolbars were extracted from home view and placed in two separated files.

--- app/controllers/indexController.js
+++ app/controllers/indexController.js
@@ -3,10 +3,14 @@ var app = angular.module('PsytestApp');
 // Controller for 'index.html'
 app.controller('IndexController', ['$scope', '$location','pbqTest', 'bdi_iiTest', 'icons',
 function($scope, $location, pbqTest, bdi_ii, icons) {
-  $scope.tests = [pbqTest, bdi_ii];
   $scope.icons = icons;
+  $scope.tests = [pbqTest, bdi_ii];
+  $scope.settings = {
+    label: '¿Dónde se guardarán los reportes?',
+    reports_dir: ''
+  };
 
-  $scope.go = function(path) {    
+  $scope.go = function(path) {
     $location.path(path);
   };
 }]);
